ecb30f7b9a9ec2742ea8c60598d19f20dbe9aed7
Carlos Cubas <carlos.cubas@informa.com>
2018-07-12 06:13:53 -0400

    fix: test out ci script

--- scripts/gcloudlogin.sh
+++ scripts/gcloudlogin.sh
@@ -2,10 +2,10 @@
 
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash; fi
 # Add gcloud to $PATH
-source /home/travis/google-cloud-sdk/path.bash.inc
+source $HOME/google-cloud-sdk/path.bash.inc
 gcloud version
 # Authentication flow
 echo $GCLOUD_KEY | base64 --decode > gcloud.json
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.json
-ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+
 
