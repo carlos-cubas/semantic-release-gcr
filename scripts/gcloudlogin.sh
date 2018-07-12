347e652e7f06ef4f667814e54bd8edaba4e19fa1
Carlos Cubas <carlos.cubas@informa.com>
2018-07-12 04:47:56 -0400

    fix: test out ci script

--- scripts/gcloudlogin.sh
+++ scripts/gcloudlogin.sh
@@ -8,3 +8,4 @@ gcloud version
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
+
