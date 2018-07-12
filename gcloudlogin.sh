5da80a7f5b1bc2a958ba50bcc7594b25b3152549
Carlos Cubas <carlos.cubas@informa.com>
2018-07-12 00:39:03 -0400

    fix: add shebang

--- gcloudlogin.sh
+++ gcloudlogin.sh
@@ -1,8 +1,9 @@
+#! /bin/bash
+
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; curl https://sdk.cloud.google.com | bash; fi
 # Add gcloud to $PATH
 source /home/travis/google-cloud-sdk/path.bash.inc
 gcloud version
-gcloud --quiet components update kubectl
 # Auth flow
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
